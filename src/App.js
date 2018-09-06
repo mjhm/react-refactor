4bc964c09e5bcb9611fcbd29b97b6b47d5ee6bdf
John McLaughlin <john.mclaughlin@originate.com>
2018-09-05 18:36:10 -0700

    setup base

--- src/App.js
+++ src/App.js
@@ -10,48 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <h1 className="App-title">Refactoring Cats and Dogs</h1>
-        <section>
-          <ul>
-            <li>
-              <dt>Order</dt>
-              <dd>Carnivora</dd>
-              <ul>
-                <li>
-                  <dt>Suborder</dt>
-                  <dd>Caniformia</dd>
-                  <ul>
-                    <li>
-                      <dt>Genus</dt>
-                      <dd>Canis</dd>
-                    </li>
-                    <li>
-                      <dt>Genus</dt>
-                      <dd>Vulpes</dd>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <dt>Suborder</dt>
-                  <dd>Feliformia</dd>
-                  <ul>
-                    <li>
-                      <dt>Genus</dt>
-                      <dd>Felis</dd>
-                    </li>
-                    <li>
-                      <dt>Genus</dt>
-                      <dd>Leopardus</dd>
-                    </li>
-                    <li>
-                      <dt>Genus</dt>
-                      <dd>Lynx</dd>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </section>
       </div>
     )
   }
